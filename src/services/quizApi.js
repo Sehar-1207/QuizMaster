e4fa3c1851cd9758b7cbffe77b08f0118d53b914
Sehar Ajmal <seharajmal452@gmail.com>
2026-04-17 14:26:40 +0500

    Category Added to api

--- src/services/quizApi.js
+++ src/services/quizApi.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
-export const fetchQuizQuestions = async (amount = 10) => {
-  const res = await axios.get("https://opentdb.com/api.php", {
-    params: {
-      amount,
-      type: "multiple",
-    },
-  });
+export const fetchQuizQuestions = async ({
+    amount = 10,
+    category = 18,
+    difficulty = "medium",
+}) => {
+    const res = await axios.get("https://opentdb.com/api.php", {
+        params: {
+            amount,
+            category,
+            difficulty,
+            type: "multiple",
+        },
+    });
 
-  return res.data.results;
+    return res.data.results;
 };
